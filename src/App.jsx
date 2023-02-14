0aca7c4de284efe0abdf5b7aa571172d15752be9
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-02-14 15:55:42 +0630

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,35 @@
-import React from "react"
-import Left from './left'
-import Right from './right'
-import Top from './top'
-import Mid from './mid'
-import Create from "./create"
+import * as React from "react";
+// import Navbar from "react-bootstrap/Navbar";
 
-const App = () => {
-  // const blackpink = [
-  //   {
-  //     id: 1,
-  //     username: "Lalisa",
-  //     description:
-  //       "Detail",
-  //     image: "https://i.pinimg.com/474x/86/ad/8c/86ad8ca08bef251166cf6e0ff9c5f913.jpg",
-     
-  //   },
-  //   {
-  //     id: 2,
-  //     username: "Kim Jisoo",
-  //     description:
-  //       "Detail",
-  //     image:
-  //       "https://i.pinimg.com/474x/29/8a/2a/298a2a26ee41eb8d864c0bddd14218b2.jpg",
-  //   },
-  //   {
-  //     id: 3,
-  //     username: "Park Chaeyoung",
-  //     description:
-  //       "Detail",
-  //     image:
-  //       "https://i.pinimg.com/474x/69/8f/41/698f416c08feb77049fcedbfe9b6de70.jpg",
-  //   },
-  //   {
-  //     id: 4,
-  //     username: "Jennie Kim",
-  //     description:
-  //       "Detail",
-  //     image:
-  //       "https://i.pinimg.com/474x/86/81/20/86812015336442c38a2b6e086125c8de.jpg",
-  //   },
-   
-  // ];
+import "bootstrap/dist/css/bootstrap.css";
+import './index.css';
 
+
+import { BrowserRouter as Router , Routes, Route} from "react-router-dom";
+
+// import EditProduct from "./components/product/edit.component";
+import Sug from "./Sug.component";
+import Index from "./Index.component";
+// import NewFeed from "./components/product/NewFeed.component"
+import User from "./User.component";
+import Auth from "./Auth.component";
+// import SignUp from "./components/product/SignUp.component";
+import Profile from "./Profile.component";
+// import CreateProduct from "./components/product/create.component";
+
+function App() {
   return (
-  <main>
-    
-    <Top/>
-    <section id="section">
-             
-             
-  <Left/>    <Mid/>   <Right/>
-    </section>
-    <Create/>
-  </main>
-  );
-};
+  <Router>
+   
+        <Routes>
+           <Route exact path='/Sug' element={<Sug />} />
+           <Route exact path='/' element={<Index />} />
+            <Route exact path='/Auth' element={<Auth/>}/>\
+            <Route exact path='/Profile' element={<Profile/>}/>
+            <Route exact path='/User' element={<User/>}/>
+        </Routes>
+       
+  </Router>);
+}
 
 export default App;
