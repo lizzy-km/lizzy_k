8ccf24310e54173802a7a9c23ed77d1fdf996809
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-02-14 08:53:24 +0630

    "the last of last"

--- src/mid.jsx
+++ src/mid.jsx
@@ -11,6 +11,7 @@ const Mid = () => {
         username: "Lalisa Manoban",
         description:
           "Detail",
+          pf: "https://i.pinimg.com/originals/08/d5/be/08d5be5926db92c51491180a211b428d.jpg",
         image: "https://i.pinimg.com/474x/86/ad/8c/86ad8ca08bef251166cf6e0ff9c5f913.jpg",
        
       },
@@ -19,6 +20,7 @@ const Mid = () => {
         username: "Kim Jisoo",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/88/d1/be/88d1beb2db936b269fc0d06407ceee23.jpg",
         image:
           "https://i.pinimg.com/474x/29/8a/2a/298a2a26ee41eb8d864c0bddd14218b2.jpg",
       },
@@ -27,6 +29,7 @@ const Mid = () => {
         username: "Park Chaeyoung",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/b5/ed/3e/b5ed3e5ed8fbfa2dceb35d1ee80c787f.jpg",
         image:
           "https://i.pinimg.com/474x/69/8f/41/698f416c08feb77049fcedbfe9b6de70.jpg",
       },
@@ -35,6 +38,7 @@ const Mid = () => {
         username: "Jennie Kim",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/67/8c/60/678c60e05f1a60ad69507a248b52b49f.jpg",
         image:
           "https://i.pinimg.com/474x/86/81/20/86812015336442c38a2b6e086125c8de.jpg",
       },{
@@ -42,6 +46,7 @@ const Mid = () => {
         username: "Hanni",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/50/a8/82/50a8828026afd8c169d51f428d65199d.jpg",
         image: "https://i.pinimg.com/originals/d5/b6/83/d5b683af4288c20e1fa86488eebd5bc3.jpg",
        
       },
@@ -50,6 +55,7 @@ const Mid = () => {
         username: "Minji",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/70/a7/f1/70a7f1512db2dc03b0eff894b9070421.jpg",
         image:
           "https://i.pinimg.com/originals/00/dc/b9/00dcb9ffa9d338683cfbbab57a249527.jpg",
       },
@@ -58,6 +64,7 @@ const Mid = () => {
         username: "Daniella",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/84/43/09/844309b3b56214e39150a67fc07adb6a.jpg",
         image:
           "https://i.pinimg.com/originals/f7/9f/7c/f79f7c41029cadbd0d990c9d97575239.jpg",
       },
@@ -66,6 +73,7 @@ const Mid = () => {
         username: "Haerin",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/4f/11/0d/4f110ddb84ef173ed5d7138b4fcd73ae.jpg",
         image:
           "https://i.pinimg.com/originals/4a/dd/0b/4add0bb47728e72da5781c4cb556c14c.jpg",
       },{
@@ -73,6 +81,7 @@ const Mid = () => {
         username: "Hyein",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/2e/26/80/2e2680fcea693ce55a86cd9d79052f81.jpg",
         image: "https://i.pinimg.com/564x/2a/84/6e/2a846e624d3bfc2d0bd03f34a9826046.jpg",
        
       },
@@ -81,6 +90,7 @@ const Mid = () => {
         username: "Winter",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/80/d3/dd/80d3ddd61d61ae37d6d74c5957404c84.jpg",
         image:
           "https://i.pinimg.com/originals/50/40/e2/5040e2d5e311959e8008a875c13f21ee.jpg",
       },
@@ -89,6 +99,7 @@ const Mid = () => {
         username: "Karina",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/736x/08/1f/0f/081f0f630381dd4d25ceba0200636d7b.jpg",
         image:
           "https://i.pinimg.com/originals/9d/5d/f8/9d5df8cc098e6a083b0721812a3ce3f3.jpg",
       },
@@ -97,6 +108,7 @@ const Mid = () => {
         username: "Ning Ning",
         description:
           "Detail",
+          pf:"https://i.pinimg.com/474x/bd/f2/eb/bdf2eb5a96a8c29683972ef2116506fc.jpg",
         image:
           "https://i.pinimg.com/originals/89/dd/1b/89dd1bc8ff27398d8cea085fece88360.jpg",
       },
@@ -162,7 +174,7 @@ const Mid = () => {
                                     <img className="hl_mp br_1 " src={gf.image} alt=""/>
                                 </div>
                                 <div className="hl_n">
-                                <img className="h_l_p pf_hover" src={gf.image} alt=""/>
+                                <img className="h_l_p pf_hover" src={gf.pf} alt=""/>
                                     <p className="hln_t">{gf.username}</p>
                                 </div>
                                 
@@ -250,7 +262,7 @@ const Mid = () => {
                 <div  className="all_p_owner">
                     <a href="">
                         <img  className="p_o_p"   
-                        src={gf.image}/>
+                        src={gf.pf}/>
                     </a>
                   
                     
