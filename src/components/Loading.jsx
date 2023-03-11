a6a5683f9f6df249a49527ceac093794f7c5b310
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-03-11 19:29:49 +0630

    fck

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cover from '../cover.png'
+
+const Loading = () => {
+  return (
+    <div>
+<img className=' blur-sm ' src={cover} alt=""/>    
+    </div>
+  )
+}
+
+export default Loading
